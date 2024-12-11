35a023b23bc047df5318ccab78330daebf1a545b
Zhefu ZHANG <a723356@gmail.com>
2024-12-11 23:04:56 +1300

    feat: finish cache

--- homework/src/hello_server/cache.rs
+++ homework/src/hello_server/cache.rs
@@ -1,22 +1,25 @@
 //! Thread-safe key/value cache.
 
-use std::collections::hash_map::{Entry, HashMap};
-use std::hash::Hash;
-use std::sync::{Arc, Mutex, RwLock};
-
+use std::clone;
+use std::collections::hash_map::{DefaultHasher, Entry, HashMap};
+use std::hash::{Hash, Hasher};
+use std::sync::{Arc, Condvar, Mutex, RwLock};
+const NUM_PARTITIONS: usize = 32;
 /// Cache that remembers the result for each key.
 #[derive(Debug)]
 pub struct Cache<K, V> {
     // todo! This is an example cache type. Build your own cache type that satisfies the
     // specification for `get_or_insert_with`.
-    inner: Mutex<HashMap<K, V>>,
+    inner: Vec<RwLock<HashMap<K, V>>>,
 }
 
 impl<K, V> Default for Cache<K, V> {
     fn default() -> Self {
-        Self {
-            inner: Mutex::new(HashMap::new()),
+        let mut inner = Vec::with_capacity(NUM_PARTITIONS);
+        for i in 0..NUM_PARTITIONS {
+            inner.push(RwLock::new(HashMap::new()));
         }
+        return Cache { inner: inner };
     }
 }
 
@@ -36,8 +39,20 @@ impl<K: Eq + Hash + Clone, V: Clone> Cache<K, V> {
     ///
     /// [`Entry`]: https://doc.rust-lang.org/stable/std/collections/hash_map/struct.HashMap.html#method.entry
     pub fn get_or_insert_with<F: FnOnce(K) -> V>(&self, key: K, f: F) -> V {
-        let mut map_guard = self.inner.lock().unwrap();
-        let entry = map_guard.entry(key.clone()).or_insert(f(key));
-        entry.clone()
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        let partition = (hasher.finish() % 32) as usize;
+        {
+            let guard = self.inner[partition].read().unwrap();
+            match guard.get(&key) {
+                Some(v) => {
+                    return v.clone();
+                }
+                None => {}
+            }
+        }
+        let mut guard = self.inner[partition].write().unwrap();
+        let v = guard.entry(key.clone()).or_insert_with(move || f(key));
+        return v.clone();
     }
 }
